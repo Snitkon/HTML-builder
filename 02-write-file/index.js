d0c1542fde8d3873b7cb66766899f286a75fe1be
Nikita <snitkonikita@gmail.com>
2022-05-24 00:20:11 +0400

    feat: 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const fs = require("fs");
 const step = require("process");
-const stream = fs.createWriteStream(path.join(__dirname, "out.txt"));
+const stream = fs.createWriteStream(path.join(__dirname, "text.txt"));
 const readline = require("readline");
 const { stdin: input, stdout: output } = require("process");
 
@@ -10,8 +10,6 @@ step.on("exit", () => step.stdout.write("Хорошего дня!\n"));
 
 step.stdout.write("Введите текст:\n");
 step.stdin.on("data", (data) => {
-  if (data.toString().trim() === "out") step.exit();
+  if (data.toString().trim() === "exit") step.exit();
   stream.write(data);
 });
-
-
